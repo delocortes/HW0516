1655850706bdd9c0fb076fcb5c81cd1a32a04ee7
delocortes <50380166+delocortes@users.noreply.github.com>
2019-05-16 12:44:19 -0500

    Add files via upload

---
+++ functionhw.js
@@ -0,0 +1,34 @@
+//BAR ACCESS
+
+if (age >= 21){   
+    console.log('ACCESS GRANTED'); 
+    }
+    
+    else if(age >= 18) {  
+        console.log('ACCESS UNTIL 10PM')
+    }
+    
+    else{  
+        console.log('ACCESS DENIED');}
+
+//BEST RAPPER 
+        var bestrapper = 'Kanye West';
+        switch (bestrapper) {
+            case 'Kanye':    
+            console.log('RIGHT ANSWER');
+            break;
+            
+            case 'Kendrick Lamar':
+            console.log('Nah homie');
+            break;
+    
+            case 'BIG SEAN':
+            console.log('He ight');
+            break;
+    
+            default:
+            console.log('WRONG ANSWER');
+            break;
+        }
+
+
