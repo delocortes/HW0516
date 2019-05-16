3242cb3ee10d811bc37ecf7e062df40303067b0b
delocortes <50380166+delocortes@users.noreply.github.com>
2019-05-16 13:26:05 -0500

    Add files via upload

--- functionhw.js
+++ functionhw.js
@@ -1,34 +1,39 @@
-//BAR ACCESS
+       console.log('Player 1 PRESS START');
+       
+       
+       var Player = 'Mario'; //Mario/Luigi
+        if(Player === 'Mario')
+        { console.log('Issa me ' + Player); }
+        else if (Player === 'Luigi') { console.log('Issa me ' + Player); }
+        else { console.log('Choose Player'); }
 
-if (age >= 21){   
-    console.log('ACCESS GRANTED'); 
-    }
-    
-    else if(age >= 18) {  
-        console.log('ACCESS UNTIL 10PM')
-    }
-    
-    else{  
-        console.log('ACCESS DENIED');}
 
-//BEST RAPPER 
-        var bestrapper = 'Kanye West';
-        switch (bestrapper) {
-            case 'Kanye':    
-            console.log('RIGHT ANSWER');
-            break;
-            
-            case 'Kendrick Lamar':
-            console.log('Nah homie');
-            break;
-    
-            case 'BIG SEAN':
-            console.log('He ight');
-            break;
-    
-            default:
-            console.log('WRONG ANSWER');
-            break;
-        }
+        console.log('A Koopa crosses your path')
+        var Action = 'Jump' // Jump/Walk
+        if(Action === 'Jump')
+        { console.log('Continue'); }
+        else if ( Action === 'Walk') { console.log('Try Again!'); }
+
+       
+     console.log('A block is above your head.')
+        var Action = 'Jump' // Jump/Walk
+        if(Action === 'Jump') 
+        { console.log('Mushroom emerges. Consumes mushroom.'); }
+        else if ( Action === 'Walk') { console.log('Continue'); }
+
+        console.log('Flag is ahead')
+        var Action = 'Jump' // Jump/Walk
+        if(Action === 'Jump') 
+        { console.log('+10,000PTS'); }
+        else if ( Action === 'Walk') { console.log('+1,000PTS'); }
+
+        console.log('A tunnel is ahead.')
+        var Action = 'Walk' // Jump/Walk
+        if(Action === 'Walk') 
+        { console.log('Stage Finished'); }
+        else if ( Action === 'Jump') { console.log('Try Again'); }
+       
+
+
 
 
